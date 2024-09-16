b1170f598b18811a93a316afb27584c1f665f193
Sergio <serprigar@gmail.com>
2024-09-16 23:58:14 +0200

    feat(BusquedaCaracteres.java): update excercice 2

--- src/main/java/org/example/BusquedaCaracteres.java
+++ src/main/java/org/example/BusquedaCaracteres.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class BusquedaCaracteres {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombreArchivo = sc.nextLine();
+        Path file = Path.of(nombreArchivo);
+        HashMap<Character, Integer> numChars = new HashMap<>();
+        try (var lector = new Scanner(file)) {
+            while (lector.hasNextLine()) {
+                String linea = sc.nextLine();
+                for (char character : linea.toCharArray()){
+                    long timesChar = linea
+                            .chars()
+                            .filter(c -> c == character)
+                            .count();
+
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
