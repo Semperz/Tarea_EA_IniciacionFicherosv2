5ffb1ba2898c89e4020a0d04dc50d326b190f507
a23sergiopg <serprigar@gmail.com>
2024-09-20 14:35:51 +0200

    refactor(Ordenaciones.java): refactor methods fileMaker and main

--- src/main/java/org/example/Ordenaciones.java
+++ src/main/java/org/example/Ordenaciones.java
@@ -5,9 +5,11 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -16,8 +18,6 @@ public class Ordenaciones {
         System.out.println("Dime la ubicacion del archivo:");
         Scanner scRoute = new Scanner(System.in);
         String rutaArchivo = scRoute.nextLine();
-        int posicionBarra = rutaArchivo.lastIndexOf("\\");
-        Path file = Path.of(rutaArchivo);
         System.out.println("Pon el numero del tipo de ordenación quieres de las siguientes:" + '\n' +
                 "1. ascendente case-sensitive" + '\n' +
                 "2. ascendente case-insensitive" + '\n' +
@@ -25,63 +25,51 @@ public class Ordenaciones {
                 "4. descendente case-insensitive");
         Scanner scSort = new Scanner(System.in);
         String sortType = scSort.nextLine();
-        System.out.println("Donde quieres crear el archivo nuevo?" + '\n');
-        Scanner scNewFile = new Scanner(System.in);
-        String routeNewFile = scNewFile.nextLine();
-        Path newFilePath = Path.of(routeNewFile);
-        try (BufferedReader lector = new BufferedReader(new FileReader(String.valueOf(file)))) {
-            String linea;
-            ArrayList<String> stringList = new ArrayList<>();
-            String resultadoOrdenado = "";
-            String newFileName = "";
-            while ((linea = lector.readLine()) != null) {
-                stringList.add(linea);
-                if (sortType.charAt(0) == '1') {
-                    resultadoOrdenado = stringList.stream().sorted().toList().toString();
-                    newFileName = "ascendente-case-sensitive-";
-                }
-                if (sortType.charAt(0) == '2') {
-                    resultadoOrdenado = stringList.stream().sorted(String.CASE_INSENSITIVE_ORDER).toList().toString();
-                    newFileName = "ascendente-case-insensitive-";
-                }
-                if (sortType.charAt(0) == '3') {
-                    resultadoOrdenado = stringList.stream().sorted(Comparator.reverseOrder()).toList().toString();
-                    newFileName = "descendente-case-sensitive-";
-                }
-                if (sortType.charAt(0) == '4') {
-                    resultadoOrdenado = stringList.stream().sorted(String.CASE_INSENSITIVE_ORDER.reversed()).toList().toString();
-                    newFileName = "descendente-case-insensitive-";
-                }
+        String nuevaRutaArchivo;
+        try {
+            List<String> linea = Files.readAllLines(Paths.get(rutaArchivo));
+            String resultadoOrdenado;
+            switch (sortType) {
+                case "1":
+                    resultadoOrdenado = linea.stream().sorted().toList().toString();
+                    nuevaRutaArchivo = rutaArchivo.replace(".txt", "-ascendente-case-sensitive.txt");
+                    break;
+                case "2":
+                    resultadoOrdenado = linea.stream().sorted(String.CASE_INSENSITIVE_ORDER).toList().toString();
+                    nuevaRutaArchivo = rutaArchivo.replace(".txt", "-ascendente-case-insensitive.txt");
+                    break;
+                case "3":
+                    resultadoOrdenado = linea.stream().sorted(Comparator.reverseOrder()).toList().toString();
+                    nuevaRutaArchivo = rutaArchivo.replace(".txt", "-descendente-case-sensitive.txt");
+                    break;
+                case "4":
+                    resultadoOrdenado = linea.stream().sorted(String.CASE_INSENSITIVE_ORDER.reversed()).toList().toString();
+                    nuevaRutaArchivo = rutaArchivo.replace(".txt", "-descendente-case-insensitive.txt");
+                    break;
+                default:
+                    throw new IllegalArgumentException("Opcion no valida");
             }
-            fileReader(fileMaker(resultadoOrdenado,newFilePath, newFileName +
-                    rutaArchivo.substring(posicionBarra + 1)));  //Ordenacion + antiguo nombre
-
+            fileReader(fileMaker(resultadoOrdenado, Path.of(nuevaRutaArchivo)));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
-    public static Path fileMaker(String resultadoOrdenado, Path newFilePath, String fileName) {
+    public static Path fileMaker(String resultadoOrdenado, Path newFilePath) {
         try {
-            Path filePath = Path.of(newFilePath.toString()+fileName);
-            try {
-                Files.createFile(filePath);
                 System.out.println("Se creó el fichero" + '\n');
-                try (BufferedWriter escritor = new BufferedWriter(new FileWriter(String.valueOf(filePath),true))) {
+                BufferedWriter escritor = Files.newBufferedWriter(newFilePath);
                     escritor.write(resultadoOrdenado);
+                    escritor.close();
                 }catch (IOException e){
                     System.err.println(e.getMessage());
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (InvalidPathException e) {
+                }catch (InvalidPathException e) {
             System.out.println("El Path introducido no es correcto");
         }
-        return Path.of(newFilePath +fileName);
+        return newFilePath;
     }
     public static void fileReader(Path newfilePath) {
-        Path p = newfilePath;
-        try (var lector = new Scanner(p)){
+        try (var lector = new Scanner(newfilePath)){
             while (lector.hasNextLine()) {
                 System.out.println(lector.nextLine());
             }
