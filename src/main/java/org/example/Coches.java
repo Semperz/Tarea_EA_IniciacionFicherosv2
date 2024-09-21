862dd1953ef9d1f0ee65eb4ad2ed13bdfe7744e2
Sergio <serprigar@gmail.com>
2024-09-21 20:00:17 +0200

    refactor(Coches.java): refactor excercice 5

--- src/main/java/org/example/Coches.java
+++ src/main/java/org/example/Coches.java
@@ -12,10 +12,7 @@ public class Coches {
     public static void main(String[] args) {
         Path filePath = Path.of("res/coches.txt");
         Map<String, String> cocheMarca= new HashMap<>();
-        System.out.println("Donde quieres crear el archivo nuevo?" + '\n');
-        Scanner scNewFile = new Scanner(System.in);
-        String routeNewFile = scNewFile.nextLine();
-        Path newFilePath = Path.of(routeNewFile);
+        String newFilePath = "res/marcas.txt";
         try (BufferedReader lector = new BufferedReader(new FileReader(String.valueOf(filePath)))) {
             String linea;
             StringBuilder formatoOrdenado = new StringBuilder();
@@ -31,29 +28,23 @@ public class Coches {
             for (HashMap.Entry<String, String> entry : cocheMarca.entrySet()){
                 formatoOrdenado.append(entry.getKey()).append(": ").append(entry.getValue()).append('\n');
             }
-            fileReader(fileMaker(formatoOrdenado.toString(), newFilePath, "marcas.txt"));
+            fileReader(fileMaker(formatoOrdenado.toString(), Path.of(newFilePath)));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    public static Path fileMaker(String formatoOrdenado, Path newFilePath, String fileName) {
+    public static Path fileMaker(String formatoOrdenado, Path newFilePath) {
         try {
-            Path filePath = Path.of(newFilePath.toString()+fileName);
-            try {
-                Files.createFile(filePath);
-                System.out.println("Se creó el fichero" + '\n');
-                try (BufferedWriter escritor = new BufferedWriter(new FileWriter(String.valueOf(filePath),true))) {
-                    escritor.write(formatoOrdenado);
-                }catch (IOException e){
-                    System.err.println(e.getMessage());
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (InvalidPathException e) {
+            System.out.println("Se creó el fichero" + '\n');
+            BufferedWriter escritor = Files.newBufferedWriter(newFilePath);
+            escritor.write(formatoOrdenado);
+            escritor.close();
+        }catch (IOException e){
+            System.err.println(e.getMessage());
+        }catch (InvalidPathException e) {
             System.out.println("El Path introducido no es correcto");
         }
-        return Path.of(newFilePath +fileName);
+        return newFilePath;
     }
     public static void fileReader(Path newfilePath) {
         try (var lector = new Scanner(newfilePath)){
