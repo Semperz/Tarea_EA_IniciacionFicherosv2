19f1d6dcfa00e40426df083e5230c2d91187b150
Sergio <serprigar@gmail.com>
2024-09-22 04:26:30 +0200

    feat(CorreccionCuestionarios.java): finish excercice 4

--- src/main/java/org/example/CorreccionCuestionarios.java
+++ src/main/java/org/example/CorreccionCuestionarios.java
@@ -1,5 +1,44 @@
 package org.example;
 
-public class CorreccionCuestionarios {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
 
+
+public class CorreccionCuestionarios {
+    public static void main(String[] args) {
+        Path filePath = Path.of("res/test.txt");
+        try (BufferedReader lector = new BufferedReader(new FileReader(String.valueOf(filePath)))) {
+            String linea;
+            String respuestasCorrectas = "";
+            String alumno;
+            String examen;
+            double notaAlumno = 0d;
+            while ((linea = lector.readLine()) != null) {
+                int posicionPrimerBS = linea.indexOf(" ");
+                if (linea.length() == 20) {
+                    respuestasCorrectas = linea;
+                }
+                if (posicionPrimerBS == -1) {
+                    continue;
+                }else{
+                    alumno = linea.substring(0, posicionPrimerBS);
+                    examen = linea.substring(posicionPrimerBS + 1);
+                }
+                for (int i = 0; i < respuestasCorrectas.length(); i++) {
+                    if (respuestasCorrectas.charAt(i) != examen.charAt(i)
+                            && examen.charAt(i) != ' ') {
+                        notaAlumno -= 0.15;
+                    } else {
+                        notaAlumno += 0.5;
+                    }
+                }
+                System.out.println(alumno + ": " + notaAlumno);
+                notaAlumno = 0;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
